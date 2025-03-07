95d98cd1fd73efa5f585af24c43b63059bce7f6f
Mem0Real <juniourmimo@gmail.com>
2025-03-07 14:43:04 +0300

    meeh

--- src/main.js
+++ src/main.js
@@ -1,54 +1,43 @@
 import "./style.css";
 
 import * as THREE from "three";
-import { GLTFLoader, OrbitControls } from "three/examples/jsm/Addons.js";
+
+import { OrbitControls } from "three/examples/jsm/Addons.js";
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x626262);
+scene.background = new THREE.Color(0x2f2f2f);
 
 // camera
 const camera = new THREE.PerspectiveCamera(
-	50,
+	75,
 	window.innerWidth / window.innerHeight,
-	0.5,
-	35
+	0.1,
+	1000
 );
-camera.position.set(-3, 2, 5);
+camera.position.set(-2, 2, 5);
+
+// cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// light
+const light = new THREE.HemisphereLight(0xeeeeff, 0x777788, 1.5);
+light.position.set(0.5, 1, 0.75);
+scene.add(light);
 
 // renderer
 const canvas = document.querySelector("canvas.threejs");
 const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-// mesh
-const geometry = new THREE.BoxGeometry(2, 2, 2);
-const material = new THREE.MeshPhongMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-
-// lights
-const ambientLight = new THREE.AmbientLight(0x404040, 5); // soft white light
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1.2);
-directionalLight.position.set(3, 4, 4);
-scene.add(ambientLight, directionalLight);
-
-// orbit control
+// controls
 const controls = new OrbitControls(camera, canvas);
-controls.maxDistance = 30;
-controls.minDistance = 7;
 controls.autoRotate = true;
 controls.enableDamping = true;
-// controls.dampingFactor = 0.1;
-
-// responsiveness
-window.addEventListener("resize", () => {
-	width = window.innerWidth;
-	height = window.innerHeight;
-	camera.aspect = width / height;
-	camera.updateProjectionMatrix();
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.render(scene, camera);
-});
+controls.dampingFactor = 1;
+scene.add(controls);
 
 function animate() {
 	controls.update();
