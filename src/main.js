3692d2b31a8b3abdb4b333b743b356f465a5a910
Mem0Real <juniourmimo@gmail.com>
2025-03-08 02:53:26 +0300

    buffer stuff

--- src/main.js
+++ src/main.js
@@ -5,20 +5,37 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x2f2f2f);
+// scene.background = new THREE.Color(0x2f2f2f);
 
 // camera
 const camera = new THREE.PerspectiveCamera(
-	75,
+	35,
 	window.innerWidth / window.innerHeight,
 	0.1,
-	1000
+	200
 );
-camera.position.set(-2, 2, 5);
+// camera.position.set(-2, 2, 5);
+camera.position.z = 5;
 
-// cube
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
+// create cube
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
+
+// create custom geometry
+const geometry = new THREE.BufferGeometry();
+const vertices = new Float32Array([
+	-1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0,
+
+	1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0,
+]);
+
+geometry.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
+const material = new THREE.MeshBasicMaterial({
+	color: "red",
+	side: THREE.DoubleSide,
+	// wireframe: true,
+});
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
@@ -34,11 +51,14 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 // controls
 const controls = new OrbitControls(camera, canvas);
-controls.autoRotate = true;
 controls.enableDamping = true;
-controls.dampingFactor = 1;
-scene.add(controls);
+// controls.autoRotate = true;
 
+window.addEventListener("resize", () => {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+});
 function animate() {
 	controls.update();
 	renderer.render(scene, camera);
